edab03daaacfe4128f9f198440cf97c5db36d822
wikitopian <wikitopian@pm.me>
2026-02-22 21:53:10 -0500

    feat: #0 gemini

--- src/core/On.js
+++ src/core/On.js
@@ -1,9 +1,22 @@
 export default class On {
   static on(parent, event, selector, fn) {
-    throw new Error('On.on: Not implemented');
+    const container = typeof parent === 'string' ? document.querySelector(parent) : (parent || document.body);
+    
+    container.addEventListener(event, (e) => {
+      const target = e.target.closest(selector);
+      if (target && container.contains(target)) {
+        fn.call(target, e, target);
+      }
+    });
   }
 
   static emit(el, event, detail) {
-    throw new Error('On.emit: Not implemented');
+    const target = typeof el === 'string' ? document.querySelector(el) : el;
+    const customEvent = new CustomEvent(event, {
+      bubbles: true,
+      cancelable: true,
+      detail
+    });
+    target.dispatchEvent(customEvent);
   }
 }
--- test/core/on.test.js
+++ test/core/on.test.js
@@ -1,15 +1,79 @@
 import test from 'node:test';
 import assert from 'node:assert';
+import { parseHTML } from 'linkedom';
 import On from '../../src/core/On.js';
 
-test('On.on: should throw "Not implemented"', (t) => {
-  assert.throws(() => {
-    On.on(null, 'click', 'selector', () => {});
-  }, { message: 'On.on: Not implemented' });
+const setupDOM = (html = '') => {
+  const { document, CustomEvent, Node } = parseHTML(`<!DOCTYPE html><html><body>${html}</body></html>`);
+  globalThis.document = document;
+  globalThis.CustomEvent = CustomEvent;
+  globalThis.Node = Node;
+  return document;
+};
+
+test('On.on: should delegate events to the selector', (t) => {
+  const dom = setupDOM('<button class="test">Click Me</button>');
+  const btn = dom.querySelector('.test');
+  let called = false;
+
+  On.on(dom.body, 'click', '.test', (e, target) => {
+    called = true;
+    assert.strictEqual(target, btn);
+  });
+
+  btn.click();
+  assert.ok(called);
 });
 
-test('On.emit: should throw "Not implemented"', (t) => {
-  assert.throws(() => {
-    On.emit(null, 'custom-event', {});
-  }, { message: 'On.emit: Not implemented' });
+test('On.on: should handle nested clicks correctly', (t) => {
+  const dom = setupDOM('<button class="test"><span>Nested</span></button>');
+  const btn = dom.querySelector('.test');
+  const span = dom.querySelector('span');
+  let called = false;
+
+  On.on(dom.body, 'click', '.test', (e, target) => {
+    called = true;
+    assert.strictEqual(target, btn);
+  });
+
+  span.click();
+  assert.ok(called);
+});
+
+test('On.on: should ignore clicks outside selector', (t) => {
+  const dom = setupDOM('<button class="test">Match</button><div class="miss">Miss</div>');
+  const miss = dom.querySelector('.miss');
+  let called = false;
+
+  On.on(dom.body, 'click', '.test', () => {
+    called = true;
+  });
+
+  miss.click();
+  assert.strictEqual(called, false);
+});
+
+test('On.on: should find parent if selector is string', (t) => {
+  const dom = setupDOM('<div id="p"><button class="c">X</button></div>');
+  let called = false;
+  
+  On.on('#p', 'click', '.c', () => {
+    called = true;
+  });
+
+  dom.querySelector('.c').click();
+  assert.ok(called);
+});
+
+test('On.emit: should dispatch CustomEvent with detail', (t) => {
+  const dom = setupDOM('<div id="target"></div>');
+  const el = dom.querySelector('#target');
+  let received = null;
+
+  el.addEventListener('my-event', (e) => {
+    received = e.detail;
+  });
+
+  On.emit(el, 'my-event', { foo: 'bar' });
+  assert.deepStrictEqual(received, { foo: 'bar' });
 });
